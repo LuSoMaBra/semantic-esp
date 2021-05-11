62721435d4967962bd6e4f632ed413febce5cede
LuSoMaBra <lusomabra@gmail.com>
2021-05-11 14:57:08 +0100

    Creating App

--- semanticesp/scrapies-esp/scrapy_curso.py
+++ semanticesp/scrapies-esp/scrapy_curso.py
@@ -3,23 +3,28 @@ import scrapy
 class UTADSpider(scrapy.Spider):
     name = "UTAD_spider"
     start_urls = ['https://www.utad.pt/estudar/inicio/licenciaturas-mestrados-integrados/']
+    urls = []
 
     def parse(self, response):
+
+        self.urls.append(response.url)
+
         SET_SELECTOR = '.rowlist'
         for curso in response.css(SET_SELECTOR):
             NOME_SELECTOR = 'li a ::text'
             LINK_SELECTOR = 'li a ::attr(href)'
-            # yield {
-            #     'nome': curso.css(NOME_SELECTOR).extract_first().replace('\n', '').replace('\t', ''),
-            #     'link': curso.css(LINK_SELECTOR).extract_first(),
-            # }
-            print(curso.css(NOME_SELECTOR).extract_first().replace('\n', '').replace('\t', ''))
+            yield {
+                'nome': curso.css(NOME_SELECTOR).extract_first().replace('\n', '').replace('\t', ''),
+                'link': curso.css(LINK_SELECTOR).extract_first(),
+            }
+            # print(curso.css(NOME_SELECTOR).extract_first().replace('\n', '').replace('\t', ''))
 
-        NEXT_PAGE_SELECTOR = '.pe-pagination a:active ::attr(href)'
+        NEXT_PAGE_SELECTOR = '.pe-pagination a ::attr(href)'
         next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
-        # print(next_page)
-        if next_page:
+        if next_page and next_page not in self.urls:
             yield scrapy.Request(
                 response.urljoin(next_page),
-                callback=self.parse
+                callback = self.parse
             )
+
+        self.logger.info('A response from %s just arrived!', response.url)
